f649250a2c4925802a5cc09aa7263e1459605cd6
Bikram Tamang <createbikram@gmail.com>
2025-11-22 14:32:28 +0545

    Demo Commit

---
+++ Documents/NetBeansProjects/2D-Duniya/src/com/bikram/duniya2d/main/Main.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.bikram.duniya2d.main;
+
+/**
+ *(2D Duniya) - 2D Game Engine
+ * Course: CS5003NI - Data Structures and Specialist Programming
+ * Module Leader: Mr. Subarna Sapkota
+ * Institution: Islington College, London Metropolitan University
+ * 
+ * Project: 2D Game Object Management System with MVC Architecture
+ * Purpose: Demonstrates data structures and algorithms through practical 
+ *          game development scenarios
+ * 
+ * @author Bikram Tamang
+ * @student_id NP01AI4A240095@islingtoncollege.edu.np
+ * @version 1.0
+ * @since November 2024
+ * @github 
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
